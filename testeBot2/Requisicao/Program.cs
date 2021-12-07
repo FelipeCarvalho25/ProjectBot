7063aa069cb8c9d64a6dba12e7265a17b5e2e22b
Joaocarvalho25 <69583375+Joaocarvalho25@users.noreply.github.com>
2021-12-06 22:57:19 -0300

    Novo Bot criado com LUIS funcionando e respostas simples
    Criado bot com base no echo bot, fazendo requisição http no LUIS e retornando a intenção da frase digitada, feita resposta básica, porém será criada classes de dialogos.

---
+++ testeBot2/Requisicao/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using testeBot2.CognitiveModels;
+
+namespace testeBot2.Requisicao
+{
+    public class Program
+    {
+        public ZeCognitiveModel model = new ZeCognitiveModel();
+        public static void get(string query)
+        {
+            var requisicaoWeb = WebRequest.CreateHttp("https://botproject.cognitiveservices.azure.com/luis/prediction/v3.0/apps/85f5328f-243e-44cb-bca2-a4b4b09e9ca4/slots/staging/predict?verbose=true&show-all-intents=true&log=true&subscription-key=334cda87740b46f990f42de1200c77cc&query="+query);
+            requisicaoWeb.Method = "GET";
+            requisicaoWeb.UserAgent = "Teste";
+            using (var resposta = requisicaoWeb.GetResponse())
+            {
+                var streamDados = resposta.GetResponseStream();
+                StreamReader reader = new StreamReader(streamDados);
+                object objResponse = reader.ReadToEnd();
+                var post = JsonConvert.DeserializeObject<ZeCognitiveModel>(objResponse.ToString());
+
+                //Console.WriteLine(post.Id + " " + post.title + " " + post.body);
+                Console.ReadLine();
+                streamDados.Close();
+                resposta.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ testeBot2/Requisicao/ZeCognitiveModel.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Bot.Builder;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace testeBot2.CognitiveModels
+{
+    public class ZeCognitiveModel
+    {
+        public string query { get; set; }
+        public string AlteredText;
+
+        public class _Prediction
+        {
+            public string topIntent { get; set; }
+        }
+        public _Prediction prediction;
+        public class _Entities
+        {
+            // Built-in entities
+            public string acao;
+            public string chamado;
+            public string incidente;
+            public string Mopp;
+            public string ocorrencia;
+            public string prazo;
+            public string problema;
+            public string refeicao;
+            public string saber;
+            public string tempo;
+            // Lists
+            public string[][] Abertura;
+            public string[][] Prazo;
+            public string[][] Saber;
+
+
+            // Instance
+            public class _Instance
+            {
+                public string[] acao;
+                public string[] chamado;
+                public string[] incidente;
+                public string[] Mopp;
+                public string[] ocorrencia;
+                public string[] prazo;
+                public string[] problema;
+                public string[] refeicao;
+                public string[] saber;
+                public string[] tempo;
+                public string[] Abertura;
+                public string[] Prazo;
+                public string[] Saber;
+            }
+            [JsonProperty("$instance")]
+            public _Instance _instance;
+        }
+        public _Entities Entities;
+
+    }
+}
